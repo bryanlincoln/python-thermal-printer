67e19d102a17dd8c708ac88c2c19dab5e700bade
Bryan Lincoln <bryanufg@gmail.com>
2019-06-22 19:58:56 -0300

    update install scripts, clean up

--- INSTALL.md
+++ INSTALL.md
@@ -1,3 +1,7 @@
 # Requirements
 - Python 3
 - Root access
+
+# Steps
+- run "sudo bash setup.sh"
+- reboot
--- setup.sh
+++ setup.sh
@@ -1,11 +1,18 @@
-echo "Criando startup.sh"
-echo "#!/bin/bash" > startup.sh
-# echo PASSWORD | sudo -S COMMAND
-echo "/usr/bin/python3 ${PWD}/ticket.py &" > startup.sh
-echo "Copiando para /etc/init.d"
-cp startup.sh /etc/init.d/startup.sh
-echo "Linkando para /etc/rc0.d"
-rm -rf /etc/rc0.d/startup.sh
-ln -s /etc/init.d/startup.sh /etc/rc0.d/
-rm -rf startup.sh
-echo "Limpando..."
+SCRIPT="/etc/rc.local"
+
+if [ ! -f $SCRIPT ]; then
+    echo "exit 0" > /etc/rc.local
+else
+    N=$(wc -l "$SCRIPT")
+    if [ "$N" == "0 /etc/rc.local" ]; then
+        echo "exit 0" > /etc/rc.local
+    fi
+fi
+
+sed -i '$ i # begin ticket generator' $SCRIPT
+sed -i '$ i _OLD_DIR=${PWD}' $SCRIPT
+sed -i '$ i cd /root/python-thermal-printer' $SCRIPT
+sed -i '$ i sudo python3 /root/python-thermal-printer/ticket.py &' $SCRIPT
+sed -i '$ i echo "Ticket generator initialized!"' $SCRIPT
+sed -i '$ i cd $_OLD_DIR' $SCRIPT
+sed -i '$ i # end ticket generator' $SCRIPT
