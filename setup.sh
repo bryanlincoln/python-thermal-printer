3b313be4ce69026b9c5d8cf6dc2b22429c89c280
Bryan Lincoln <bryanufg@gmail.com>
2019-06-22 20:03:08 -0300

    more generic setup

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 SCRIPT="/etc/rc.local"
-
+HERE=${PWD}
 if [ ! -f $SCRIPT ]; then
     echo "exit 0" > /etc/rc.local
 else
@@ -11,8 +11,8 @@ fi
 
 sed -i '$ i # begin ticket generator' $SCRIPT
 sed -i '$ i _OLD_DIR=${PWD}' $SCRIPT
-sed -i '$ i cd /root/python-thermal-printer' $SCRIPT
-sed -i '$ i sudo python3 /root/python-thermal-printer/ticket.py &' $SCRIPT
+sed -i '$ i cd '"$HERE" $SCRIPT
+sed -i '$ i sudo python3 ticket.py &' $SCRIPT
 sed -i '$ i echo "Ticket generator initialized!"' $SCRIPT
 sed -i '$ i cd $_OLD_DIR' $SCRIPT
 sed -i '$ i # end ticket generator' $SCRIPT
