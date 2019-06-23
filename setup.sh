82e69b49e08505a8fd6099c6740507459fcf31a1
Bryan Lincoln <bryanufg@gmail.com>
2019-06-23 15:21:18 -0300

    Messages on setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,28 @@
 SCRIPT="/etc/rc.local"
 HERE=${PWD}
+
+echo "Criaremos uma entrada para executar $HERE/ticket.py no arquivo /etc/rc.local"
+echo ""
+
 if [ ! -f $SCRIPT ]; then
+    echo "/etc/rc.local não existe. Criando..."
+    echo ""
     echo "exit 0" > /etc/rc.local
 else
     N=$(wc -l "$SCRIPT")
     if [ "$N" == "0 /etc/rc.local" ]; then
+        echo "/etc/rc.local existe mas não tem conteúdo. Populando..."
+        echo ""
         echo "exit 0" > /etc/rc.local
+    else
+        echo "/etc/rc.local não está vazio. Criaremos um backup chamado /etc/rc.local.old"
+        echo ""
+        cp /etc/rc.local /etc/rc.local.old
     fi
 fi
+
+echo "Adicionando entradas ao /etc/rc.local..."
+echo ""
 
 sed -i '$ i # begin ticket generator' $SCRIPT
 sed -i '$ i _OLD_DIR=${PWD}' $SCRIPT
@@ -16,3 +31,5 @@ sed -i '$ i sudo python3 ticket.py &' $SCRIPT
 sed -i '$ i echo "Ticket generator initialized!"' $SCRIPT
 sed -i '$ i cd $_OLD_DIR' $SCRIPT
 sed -i '$ i # end ticket generator' $SCRIPT
+
+echo "Pronto. Você pode reiniciar o dispositivo."
